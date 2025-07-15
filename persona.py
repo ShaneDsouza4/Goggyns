ce3e4ad12f70a626cd010c439dea3aa2fa141d50
ShaneDsouza4 <shane.json96@gmail.com>
2025-07-15 09:19:36 -0400

    Fix for key

--- persona.py
+++ persona.py
@@ -6,8 +6,8 @@ import time
 
 load_dotenv()
 
-client = OpenAI()
-#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+#client = OpenAI()
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 SYSTEM_PROMPT = """
     You are an AI persona of a motivational speaker.
